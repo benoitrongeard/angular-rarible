26ca118ec08423c57a7393408e54ec87978fbb9b
Benoit Rongeard <benoit.rongeard@gmail.com>
2022-05-26 18:32:09 +0200

    Adding config template

---
+++ src/environments/config-template.ts
@@ -0,0 +1,10 @@
+import { Env } from ".";
+
+export const defaultEnv: Env = {
+    production: false,
+    env: "dev",
+    moralis: {
+        appId: "XXXXX",
+        serverUrl: "YYYYY"
+    }
+};
